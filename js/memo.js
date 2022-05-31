8cb9f61a5d549b63a97df5da12efd0b4a1881cee
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-06-01 00:26:00 +0900

    Javascript: memo

---
+++ js/memo.js
@@ -0,0 +1,6 @@
+function addQuiz() {
+  window.open("quiz_popup.html", "quiz_add", "width=400, height=300, left=100, top=50");
+}
+function addMemo() {
+  window.open("memo_popup.html", "memo_add", "width=400, height=300, left=100, top=50");
+}
