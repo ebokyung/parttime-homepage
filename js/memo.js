0daca76b3aca2240182b8ca8449808b94a67077c
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-06-02 18:08:37 +0900

    Javascript: memo_popup

--- js/memo.js
+++ js/memo.js
@@ -1,6 +1,20 @@
 function addQuiz() {
-  window.open("quiz_popup.html", "quiz_add", "width=400, height=300, left=100, top=50");
+  window.open("quiz_popup.html", "quiz_add", "width=400, height=200, left=200, top=100");
 }
 function addMemo() {
-  window.open("memo_popup.html", "memo_add", "width=400, height=300, left=100, top=50");
+  window.open("memo_popup.html", "memo_add", "width=400, height=200, left=200, top=100");
 }
+
+function displayList() {
+  let tableTag = document.getElementById('tbody__tag');
+  tableTag.innerHTML = '';
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let val = localStorage.getItem(key);
+    // console.log(key + ' : ' + val);
+    tableTag.innerHTML += `<tr>
+    <td><input type="checkbox"> ${val}</td>
+  </tr>`
+  }
+}
+
