2678f2c863b56d22aa8faa150602d68d04ea4795
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-06-08 00:04:02 +0900

    Javascript: quiz

--- js/memo.js
+++ js/memo.js
@@ -1,7 +1,7 @@
-function addQuiz() {
+function openQuizPopup() {
   window.open("quiz_popup.html", "quiz_add", "width=400, height=200, left=200, top=100");
 }
-function addMemo() {
+function openMemoPopup() {
   window.open("memo_popup.html", "memo_add", "width=400, height=200, left=200, top=100");
 }
 
@@ -15,6 +15,33 @@ function displayList() {
     <td><input type="checkbox"> ${val}</td>
   </tr>`
   }
+}
+
+function appendQuiz(q, a) {
+  let divTag = document.getElementById("quiz__container");
+
+  let div = document.createElement('div');
+
+  let p1 = document.createElement('p');
+  p1.innerHTML = 'Q. ' + q;
+  div.appendChild(p1);
+
+  let btn = document.createElement('button');
+  btn.setAttribute('onclick', 'toggle(this)');
+  btn.innerHTML = '답보기';
+  div.appendChild(btn);
+
+  let p2 = document.createElement('p');
+  p2.setAttribute('style', 'display:none');
+  p2.innerHTML = a;
+  div.appendChild(p2);
+
+  console.log(div);
+
+  divTag.appendChild(div);
+
+  let hr = document.createElement('hr');
+  divTag.appendChild(hr);
 }
 
 function toggle(obj) {
