4a74f555d6d9983bd20b3bae15bec267c1a19513
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-06-10 11:51:32 +0900

    Javascript: home - vlog section

--- js/home.js
+++ js/home.js
@@ -10,6 +10,33 @@ function displayMemo() {
     let val = localStorage.getItem(key);
     tableTag.innerHTML += `<tr>
     <td><input type="checkbox"> ${val}</td>
-  </tr>`
+  </tr><hr>`
   }
 }
+
+
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+let videoContainer = document.querySelector('#video__container');
+let videoArticles = document.querySelectorAll("#video__container article");
+
+const size = videoArticles[0].clientWidth;
+let length = videoArticles.length;
+let cnt = 1;
+
+prevBtn.addEventListener("click", function () {
+  if (cnt <= 1) {
+    cnt = 0;
+  } else {
+    cnt--;
+  }
+  videoContainer.style.transform = "translateX(" + (-size * cnt) + "px)"
+});
+nextBtn.addEventListener("click", function () {
+  videoContainer.style.transform = "translateX(" + (-size * cnt) + "px)"
+  if (cnt >= length - 1) {
+    cnt = length - 1;
+  } else {
+    cnt++;
+  }
+});
